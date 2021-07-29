16da74f834f0a34265e88f82047bac8885b9a7af
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-29 14:26:51 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -15,23 +15,38 @@ function onSearch(e) {
   refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = '';
 
-  const searchQuery = e.target.value;
+  const searchQuery = e.target.value.trim();
+
+  if (searchQuery.length === 0) {
+    addClassVisuallyHidden();
+    return;
+  }
 
   API.fetchCountries(searchQuery).then(renderCardCountry).catch(onFetchError);
 }
 
 function renderCardCountry(country) {
   if (country.length === 1) {
-    const markup = countryInfoTpl(country[0]);
+    const markup = countryInfoTpl(...country);
     refs.countryInfo.insertAdjacentHTML('beforeend', markup);
   } else if (country.length >= 2 && country.length <= 10) {
     const markup = countryListTpl(country);
+    removeClassVisuallyHidden();
     refs.countryList.insertAdjacentHTML('beforeend', markup);
   } else if (country.length > 10) {
+    addClassVisuallyHidden();
     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
   }
 }
 
 function onFetchError() {
   Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function addClassVisuallyHidden() {
+  refs.countryList.classList.add('visually-hidden');
+}
+
+function removeClassVisuallyHidden() {
+  refs.countryList.classList.remove('visually-hidden');
 }
